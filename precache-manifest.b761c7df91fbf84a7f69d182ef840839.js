e7745b1cf5c58c2d37155f54cb119427a98588cd
Peggy Ekwalla <pekwalla@gmail.com>
2019-07-21 23:15:34 -0400

    Updates

---
+++ precache-manifest.b761c7df91fbf84a7f69d182ef840839.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68162dccad071c4e675c649b4156dcc",
+    "url": "/cgame/index.html"
+  },
+  {
+    "revision": "b25a78f1edc7c009b1a1",
+    "url": "/cgame/static/css/main.4839b3c9.chunk.css"
+  },
+  {
+    "revision": "e3348f8a417b9ca01165",
+    "url": "/cgame/static/js/2.b71c785f.chunk.js"
+  },
+  {
+    "revision": "b25a78f1edc7c009b1a1",
+    "url": "/cgame/static/js/main.1354a3d7.chunk.js"
+  },
+  {
+    "revision": "74a03c0ffe261ec212aa",
+    "url": "/cgame/static/js/runtime~main.02a299db.js"
+  }
+]);
